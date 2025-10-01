57b7f4ea7ace06861b41b3700bcb20d9983e4d4c
frankpulido <frankpulido@me.com>
2025-10-01 09:26:39 +0200

    tiny fix in notification model and adding Notion link to README

--- laravel/app/Models/Notification.php
+++ laravel/app/Models/Notification.php
@@ -14,7 +14,6 @@ class Notification extends Model
     public const VALID_TYPES = ['in-app', 'sms', 'mail', 'push'];
     protected $table = 'notifications';
     protected $fillable = [
-        'publisher_id',
         'publisher_list_id',
         'type' => 'string',
         'title' => 'string',
