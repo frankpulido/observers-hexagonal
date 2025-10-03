f67709ea51be4cd820349f2df3b109581fe7fe5c
frankpulido <frankpulido@me.com>
2025-10-03 20:16:32 +0200

    index to test railway endpoint

--- laravel/app/Http/Controllers/SubscriberController.php
+++ laravel/app/Http/Controllers/SubscriberController.php
@@ -2,9 +2,15 @@
 declare(strict_types=1);
 namespace App\Http\Controllers;
 
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
-    //
+    public function index()
+    {
+        $subscribers = Subscriber::all();
+        return response()->json([
+            'subscribers' => $subscribers], 200);
+    }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,12 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscriberController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/subscribers', [SubscriberController::class, 'index'])->name('subscribers.index');
