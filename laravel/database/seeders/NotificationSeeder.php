adcd7cb5ec158c3815a7cb934998eb29be3bf765
frankpulido <frankpulido@me.com>
2025-10-16 17:58:07 +0200

    notification seeder issue with fakerfactory

--- laravel/database/seeders/NotificationSeeder.php
+++ laravel/database/seeders/NotificationSeeder.php
@@ -20,8 +20,8 @@ class NotificationSeeder extends Seeder
         $faker = FakerFactory::create();
         $publisherListIds = PublisherList::all()->pluck('id')->toArray();
         foreach($publisherListIds as $publisherListId){
-            $type = $faker->randomElement(Notification::VALID_TYPES);
-            $message = $faker->text(200);
+            $type = $faker->create()->randomElement(Notification::VALID_TYPES);
+            $message = $faker->create()->text(200);
             Notification::create([
                 'publisher_list_id' => $publisherListId,
                 'type' => $type,
